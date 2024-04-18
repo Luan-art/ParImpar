6fde15df36e581db53dcce7df8565a77372d6034
Luan-art <luanlf2003@gmail.com>
2024-04-18 15:51:26 -0300

    Add project files.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+float number;
+
+Console.WriteLine("Escreva um numéro");
+number = float.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("par");
+}
+else
+{
+    Console.WriteLine("impar");
+}
